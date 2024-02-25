fb7babeebe3af1c4799faeed75a46b7e9c647546
Rahul Sharma <rahulsharma.cse00@gmail.com>
2024-02-25 12:38:14 +0530

    onclick function

--- src/User.js
+++ src/User.js
@@ -1,8 +1,17 @@
 import './User.css';
 function User() {
+    let value = "React";
+    let data = "New Value";
+
+    function Clicked() {
+        // let data = "New Value";
+        alert("Hello, Click Me")
+    }
     return (
         <div>
-            <h1>React JS</h1>
+            <h1>{value}</h1>
+            <h1>{data}</h1>
+            <button onClick={Clicked}>Click Me</button>
         </div>
     );
 }
