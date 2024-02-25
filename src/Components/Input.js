9fa00187539a83b64ea4a2b26a3147e02c5c1227
Rahul Sharma <rahulsharma.cse00@gmail.com>
2024-02-25 22:38:49 +0530

    input box data print and hide

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -2,13 +2,20 @@ import './Input.css';
 import React, { useState } from 'react';
 
 export default function Input() {
+    const [data, setdata] = useState(null)
+    const [print, setprint] = useState(false)
+
     function getData(val) {
-        console.warn(val.target.value)
+        setdata(val.target.value)
+        setprint(false)
+        // console.warn(val.target.value)
     }
     return (
-        <div>
-            <h1>Get Input Box Value</h1>
+        <div>{print ? <h2>{data}</h2> : null}
             <input type="text" onChange={getData} />
+            <h1>Get Input Box Value</h1>
+
+            <button onClick={()=>{setprint(true)}}>Print Data</button>
         </div>
     )
 }
